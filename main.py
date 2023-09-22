0bc835ba7b7f6701428527008a9bb0cc68b856d6
LisourSane <homesikgold@wp.pl>
2023-09-22 11:58:04 +0200

    Game's finished

--- main.py
+++ main.py
@@ -1,50 +1,47 @@
 import random
 import numpy as np
-import matplotlib.pyplot as plp
 
 List = [i for i in range(100,1000)]
 onten = [i for i in range (1,10)]
 zeten = [i for i in range (0,10)]
 numb = random.choice(List)
-print(numb)
-first = round((numb - numb%100)/100)
-second = round((numb%100 - numb%10)/10)
-third = numb%10
+#print(numb)
+first = round((numb - numb % 100)/100)
+second = round((numb % 100 - numb % 10)/10)
+third = numb % 10
 numbl = np.array([first, second, third])
-print(first, second, third)
+j = 1
+#print(first, second, third)
 print("Please, guess a three digit number.")
 guess = int(input())
-i = 1
-while guess != numb or i == 10:
+
+while guess != numb and j < 10:
     while guess < 100 or guess > 999:
         print("This isn't three digit number. Please, guess THREE digit number")
         guess = int(input())
-    print("Guess", i)
-    i = i + 1
+    print("Guess", j)
+    j = j+1
     guess1 = round((guess - guess % 100) / 100)
     guess2 = round((guess % 100 - guess % 10) / 10)
     guess3 = guess % 10
-    print(guess1, guess2, guess3)
+    #print(guess1, guess2, guess3)
     guessl = np.array([guess1, guess2, guess3])
     res = [i for i in guessl-numbl if i == 0]
     res1 = ['0', '0', '0']
     for i in range(0, 3):
         if guessl[i] == numbl[i]:
             res1[i] = guessl[i]
-    print(type(res1))
+    #print(res1)
     pico = [i for i in guessl if i in numbl]
     pico = list(pico)
     a = len(res)
     pico1 = [i for i in pico if i not in res1]
-    b = len(pico)
+    #print(pico1)
+    b = len(pico1)
     if len(res) == 2:
         print("Fermi "*a)
-    elif len(res) == 2:
-        print("Fermi, Fermi")
     elif len(pico) != 0:
-        print("Pico "*b)
-    elif len(pico) == 2:
-        print("Pico, Pico")
+        print("Fermi "*a, "Pico "*b)
     else:
         print("Bagels")
     guess = int(input())
